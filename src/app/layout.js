7cebbe4e607e11eb147a1ab29394641f58bc9b27
taynara veloso <taynarasantos2.3@hotmail.com>
2024-07-11 20:58:37 -0300

    :star: layout: font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Nunito_Sans } from "next/font/google";
+import "/src/styles/global.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunito_sans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito_sans.className}>{children}</body>
     </html>
   );
 }
